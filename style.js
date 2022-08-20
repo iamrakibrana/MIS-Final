aabf2d1de31f3cb4d63a44a70a136decb2e54f83
iamrakibrana <iamrakibranaavro@gmail.com>
2022-08-20 10:34:31 +0600

    last commite

---
+++ style.js
@@ -0,0 +1,48 @@
+function Meal() {
+    var searchText = document.getElementById("search").value ; 
+    
+  
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`; 
+  
+    fetch (url)
+    .then(res => res.json()   )
+    .then(data => display(data)   ); 
+  
+  // clearing search box and previous content 
+     document.getElementById("search").value = " "; 
+     document.getElementById("container").textContent = " ";
+  
+  }
+  
+  function display (data){
+  
+   var oldContent = document.getElementById("container")
+   var userLimit = document.getElementById("limit").value ;
+    userLimit=parseInt(userLimit);
+
+    // clearing search box and previous content 
+    document.getElementById("limit").value = " "; 
+     document.getElementById("container").textContent = " ";
+
+
+   for (var a=0; a<userLimit; a++){
+    
+     var newDiv = document.createElement("div");
+     newDiv.innerHTML = `
+     <div class="card" style="width: 18rem;">
+    <img src="${data.meals[a].strMealThumb}" class="card-img-top" alt="..."> </br>
+    <div class="card-body">
+      <h4 class="card-title"><b>${data.meals[a].strMeal}</h4></b>
+      <p class="card-text">
+      <h3><i>Cooking Instructions:<i></h3>
+      ${data.meals[a].strInstructions}
+      </p>
+      <a href="https://www.themealdb.com/" class="btn btn-primary">Visit Site</a>
+    </div>
+  </div> ` ;
+  
+      oldContent.appendChild(newDiv);
+  
+   }
+  
+  }
